2b570620fda86c96670bc4f2a24ea79ef2ea474a
Luiz Guilherme de Melo <luiz.melo@alunos.ifsuldeminas.edu.br>
2022-06-06 15:49:05 -0300

    Update index.php

--- Aula03Max/index.php
+++ Aula03Max/index.php
@@ -11,14 +11,10 @@
     <h1 style="text-align:center"> Aulas de PHP</h1>
     <?php
 
-      if(isset($_POST['palindromo'])) $palavra = $_POST['palindromo'];
-      
-      if(isset($palavra)){
-        if($palavra == strrev($palavra)){
-          $isset = "";
-        }
-        echo isset($isset) ? "<h1>É Palíndromo!</h1>" : "<h1>Não é Palíndromo!</h1>";
-      }
+      if(isset($_POST['palindromo'])){
+        $palavra = $_POST['palindromo'];
+        echo $palavra == strrev($palavra) ? "<h1>É Palíndromo!</h1>" : "<h1>Não é Palíndromo!</h1>";
+      } 
     ?>
     <form action="index.php" method="POST">
       <input type="text" placeholder="Digite a palavra!" name="palindromo">
